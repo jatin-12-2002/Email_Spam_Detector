88033f503385935c2f9e760cd8672e968b8c73f2
kdot313 <165220190+kdot313@users.noreply.github.com>
2024-10-19 22:27:12 +0000

    app done

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-from flask import Flask, request, jsonify,render_template
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 import os
-from flask_cors import CORS, cross_origin
 from spams import prediction
 
-os.putenv('LANG', 'en_US.UTF-8')
-os.putenv('LC_ALL', 'en_US.UTF-8')
-
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 
+# Home route to serve the HTML page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Route to handle predictions
 @app.route("/predict", methods=['POST'])
-@cross_origin()
-def predictRoute():
-    data = request.json["data"]
-    p = prediction(data)
-    print(p)
-    print(type(p))
-    return {"Result": str(p)}
+def predict():
+    data = request.json.get("data", [])
+    if data:
+        prediction_result = prediction(data)
+        print(prediction_result)
+        print(type(prediction_result))
+        return jsonify({"Result": str(prediction_result)})
+    else:
+        return jsonify({"Result": "No data provided"}), 400
 
-
-#port = int(os.getenv("PORT"))
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=port)
     app.run(host='0.0.0.0', port=4000, debug=True)
